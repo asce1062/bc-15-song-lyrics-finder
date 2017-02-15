87e090be979209edd6ecc94a052f3d3fca92ad9f
asce1062 <tnkratos@gmail.com>
2017-02-15 23:37:24 +0300

    Start adding comments to code
    http://pythoncentral.io/introductory-tutorial-python-sqlalchemy/

--- song_lyrics_finder.py
+++ song_lyrics_finder.py
@@ -14,9 +14,18 @@ Session = sessionmaker()
 
 # ... later
 engine = create_engine('sqlite:///song_lyrics.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
 Session.configure(bind=engine)
 
 # instantiate database transactions
+# A Session() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
 session = Session()
 
 # Static variables.
@@ -113,6 +122,6 @@ def song_clear():
         Base.metadata.drop_all(bind=engine)
         print colored.red("All tables dropped successfully.", bold=12)
         Base.metadata.create_all(bind=engine)
-        print colored.green("all tables recreated successfully", bold=12)
+        print colored.green("All tables recreated successfully", bold=12)
     except:
         session.rollback()
