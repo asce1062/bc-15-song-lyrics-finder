ce56f336652344f825d7b0a9cb74f872642eed61
asce1062 <tnkratos@gmail.com>
2017-02-15 10:37:05 +0300

    Add function to find song based on artists
    Print out the artist names and the Song IDs, which will be required for the ID based search

--- song_lyrics_finder.py
+++ song_lyrics_finder.py
@@ -4,6 +4,7 @@ import urllib2
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from prettytable import PrettyTable
 
 from models import SongLyricsFinder
 
@@ -21,6 +22,35 @@ sess = Session()
 apikey_musixmatch = 'bbc2cd1c9f66b9294d130add1b3534c4'
 apiurl_musixmatch = 'http://api.musixmatch.com/ws/1.1/'
 db_name = 'song_lyrics.db'
+
+
+# Returns a list of songs that match the criteria.
+def song_find_name(search_name):
+    querystring = apiurl_musixmatch + "track.search?q_track=" + search_name + "&apikey=" + apikey_musixmatch + "&json"
+    try:
+        request = urllib2.Request(querystring)
+        # timeout set to 4 seconds; automatically retries
+        response = urllib2.urlopen(request, timeout=4)
+        raw = response.read()
+        json_obj = json.loads(raw.decode("utf-8"))
+        body = len(json_obj["message"]["body"])
+        song_list = []
+        track_table = PrettyTable(['Track Id', 'Track Name', 'Primary Artist'])
+        for result in body:
+            song_details = []
+            song_id = result['track']['track_id']
+            song_name = result['track']['track_name']
+            artist_name = result['track']['artist_name']
+            song_details.insert(0, song_id)
+            song_details.insert(1, song_name)
+            song_details.insert(2, artist_name)
+            song_list.append(song_details)
+            track_table.add_row([song_id, song_name, artist_name])
+        print track_table
+    except socket.timeout:
+        print 'connection time out, try again'
+
+song_find_name('system of a down')
 
 
 def song_find(search_term):
